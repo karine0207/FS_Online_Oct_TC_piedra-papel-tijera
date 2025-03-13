c33e8f485a2723320e4043d2bc63def1a1ab7449
Karine gil <karine-07@hotmail.es>
2025-03-13 17:40:44 +0100

    proyecto basico sin terminar

---
+++ js/script.js
@@ -0,0 +1,29 @@
+const opciones = ["Piedra", "Papel", "Tijeras"];
+
+let puntosUsuarios = document.getElementById("contador-usuario");
+
+let puntosOrdenador = document.getElementById("contador-ordenador");
+
+let resultados = document.getElementById("resultados");
+
+
+const botones =  document.querySelectorAll("#boton-jugada");
+ 
+botones.addEventListener("click",insertar);
+
+function jugar () {
+    
+    opciones.forEach()
+    
+}
+function obtenerResultado (){
+
+}
+function mostrarResultado (){
+
+}
+function actualizarPuntuacion (){
+
+}
+
+
